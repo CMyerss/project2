38f0b0af727353d4ab319c5b9c94c9f1fe4ae440
CMyers1202 <myerschase00@gmail.com>
2021-10-25 17:24:13 -0400

    making personal look like group

---
+++ create_champions.sql
@@ -0,0 +1,8 @@
+-- Remove any existing database and user.
+DROP DATABASE IF EXISTS champions;
+DROP USER IF EXISTS champions_user@localhost;
+
+-- Create champions database and user. Ensure Unicode is fully supported.
+CREATE DATABASE champions CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+CREATE USER champions_user@localhost IDENTIFIED WITH mysql_native_password BY 'REDACTED';
+GRANT ALL PRIVILEGES ON champions.* TO champions_user@localhost;
---
+++ creation.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fs = require('fs');
+const mysql = require('mysql');
+
+const credentials = JSON.parse(fs.readFileSync('credentials.json', 'utf8'));
+const connection = mysql.createConnection(credentials);
+
+const create_champion = express();
+
+connection.connect(error => {
+    if (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  });
+
+const selectQuery = 'SELECT * FROM champions';
+connection.query(selectQuery, (error, rows) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(rows);
+  }
+});
