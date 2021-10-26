88c819abc5c22c77ca97361f2be50161c57bcc82
CMyers1202 <myerschase00@gmail.com>
2021-10-26 17:56:06 -0400

    trying again.

--- new_champions.sql
+++ new_champions.sql
@@ -1,3 +1,5 @@
-INSERT INTO champions(c_name, c_type, c_role, c_desc) VALUES ("Aatrox", "Fighter", "Top", "A moderately difficult character.")
-INSERT INTO champions(c_name, c_type, c_role, c_desc) VALUES ("Jayce", "Fighter", "Top", "A moderately difficult character.")
-INSERT INTO champions(c_name, c_type, c_role, c_desc) VALUES ("Tahm Kench", "Support", "Any", "An easily played character.")
+USE champions;
+
+INSERT INTO champions(c_name, c_type, c_role, c_desc) VALUES ("Aatrox", "Fighter", "Top", "A moderately difficult character.");
+INSERT INTO champions(c_name, c_type, c_role, c_desc) VALUES ("Jayce", "Fighter", "Top", "A moderately difficult character.");
+INSERT INTO champions(c_name, c_type, c_role, c_desc) VALUES ("Tahm Kench", "Support", "Any", "An easily played character.");
