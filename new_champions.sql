ace5cf3a6833c9f933280f57a7958fa3a694b006
CMyers1202 <myerschase00@gmail.com>
2021-10-26 17:49:20 -0400

    Added insert statements for the tabel.

---
+++ new_champions.sql
@@ -0,0 +1,3 @@
+INSERT INTO champions(c_name, c_type, c_role, c_desc) VALUES ("Aatrox", "Fighter", "Top", "A moderately difficult character.")
+INSERT INTO champions(c_name, c_type, c_role, c_desc) VALUES ("Jayce", "Fighter", "Top", "A moderately difficult character.")
+INSERT INTO champions(c_name, c_type, c_role, c_desc) VALUES ("Tahm Kench", "Support", "Any", "An easily played character.")
