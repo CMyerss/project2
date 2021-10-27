f22c3e6f9230911de4fbf92d2486ad3b96b9ed47
CMyers1202 <myerschase00@gmail.com>
2021-10-26 21:03:18 -0400

    Making mine look like group work.

--- creation.js
+++ creation.js
@@ -1,11 +1,12 @@
-//const express = require('express');
+const express = require('express');
 const fs = require('fs');
 const mysql = require('mysql');
 
 const credentials = JSON.parse(fs.readFileSync('credentials.json', 'utf8'));
 const connection = mysql.createConnection(credentials);
 
-//const create_champion = express();
+const service = express();
+service.use(express.json());
 
 connection.connect(error => {
     if (error) {
@@ -22,3 +23,184 @@ connection.query(selectQuery, (error, rows) => {
     console.log(rows);
   }
 });
+
+function rowToChampions(row) {
+  return {
+    id: row.id,
+    name: row.c_name,
+    type: row.c_type,
+    lane: row.c_role,
+    desc: row.c_desc,
+  };
+}
+
+// Select function for when we have wildcards to account for.
+function select(query, params, response) {
+  connection.query(query, params, (error, rows) => {
+    if (error) {
+      response.status(500);
+      response.json({
+        ok: false,
+        results: error.message,
+      });
+    } else {
+      response.json({
+        ok: true,
+        results: rows.map(rowToChampions),
+      });
+    }
+  });
+
+}
+
+// Select function for when there are no wildcards to account for.
+function selectAll(query, response) {
+  connection.query(query, (error, rows) => {
+    if (error) {
+      response.status(500);
+      response.json({
+        ok: false,
+        results: error.message,
+      });
+    } else {
+      response.json({
+        ok: true,
+        results: rows.map(rowToChampions),
+      });
+    }
+  });
+}
+
+// Inserts a new champion into the database.
+function insert(query, params, response) {
+  connection.query(query, params, (error, result) => {
+    if (error) {
+      response.status(500);
+      response.json({
+        ok: false,
+        results: error.message,
+      });
+    } else {
+      response.json({
+        ok: true,
+        results: result.insertId,
+      });
+    }
+  });
+}
+
+/****************** SELECTS ********************/
+
+// get all the champions.
+service.get('/champions', (request, response) => {
+  const query = 'SELECT * FROM champions ORDER BY c_role';
+  selectAll(query, response);
+});
+
+// get a certain champion from the database. 
+service.get('/:champion', (request, response) => {
+  const param = [request.params.champion];
+  const query = 'SELECT * FROM champions WHERE c_name = ?';
+
+  select(query, param, response);
+});
+
+// get a certain role from the database. 
+service.get('/:role', (request, response) => {
+  const param = [request.params.role];
+  const query = 'SELECT * FROM champions WHERE c_role = ?';
+
+  select(query, param, response);
+});
+
+// get a certain type from the database. 
+service.get('/:type', (request, response) => {
+  const param = [request.params.type];
+  const query = 'SELECT * FROM champions WHERE c_type = ?';
+
+  select(query, param, response);
+});
+
+/****************** POST *****************/
+
+service.post('/champions', (request, response) => {
+
+  if (request.body.hasOwnProperty('c_name') &&
+  request.body.hasOwnProperty('c_type') &&
+  request.body.hasOwnProperty('c_role') &&
+  request.body.hasOwnProperty('c_desc')) {
+
+    const param = [
+      request.body.c_name,
+      request.body.c_type,
+      request.body.c_role,
+      request.body.c_desc
+    ];
+
+    const query = 'INSERT INTO champions(c_name, c_type, c_role, c_desc) VALUES (?, ?, ?, ?)';
+
+    insert(query, param, response);
+    
+  } else {
+    response.status(400);
+    response.json({
+      ok: false,
+      results: 'Insufficient champion data.',
+    });
+  }
+});
+
+/****************** UPDATE *********************/
+service.patch('/champions/:id', (request, response) => {
+  const param = [
+    request.body.c_name,
+    request.body.c_type,
+    request.body.c_role,
+    request.body.c_desc,
+    parseInt(request.params.id),
+  ];
+
+  const query = 'UPDATE champions SET c_name = ?, c_type = ?, c_role = ?, c_desc = ? WHERE id = ?';
+  connection.query(query, parameters, (error, result) => {
+    if (error) {
+      response.status(404);
+      response.json({
+        ok: false,
+        results: error.message,
+      });
+    } else {
+      response.json({
+        ok: true,
+      });
+    }
+  });
+});
+
+
+/****************** DELETE *********************/
+service.delete('/champions/:id', (request, response) => {
+  const parameters = [parseInt(request.params.id)];
+
+  const query = 'DELETE FROM champions WHERE id = ?';
+  connection.query(query, parameters, (error, result) => {
+    if (error) {
+      response.status(404);
+      response.json({
+        ok: false,
+        results: error.message,
+      });
+    } else {
+      response.json({
+        ok: true,
+      });
+    }
+  });
+});
+
+
+/****************** CONNECTION *****************/
+const port = 5001;
+service.listen(port, () => {
+  console.log(`We're live in port ${port}!`);
+});
+
